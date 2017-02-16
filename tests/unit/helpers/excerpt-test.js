f673fe8fdd7f66c924083935de708fef20853ea5
drunkmonky <drunkmonky+github@gmail.com>
2017-02-15 19:36:18 -0800

    add helpers and utils

---
+++ tests/unit/helpers/excerpt-test.js
@@ -0,0 +1,12 @@
+
+import { excerpt } from 'blog/helpers/excerpt';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | excerpt');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = excerpt([42]);
+  assert.ok(result);
+});
+
